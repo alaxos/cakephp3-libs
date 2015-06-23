08f8a0cfefaf3671823596b4d3ce53902e4a4395
Nicolas Rod <nico@alaxos.com>
2015-06-23 10:27:51 +0200

    TimezonedTrait: added possibility to have a default display timezone in addition to the configured display timezone

--- src/Model/Entity/TimezonedTrait.php
+++ src/Model/Entity/TimezonedTrait.php
@@ -9,11 +9,20 @@ trait TimezonedTrait
     {
         $value = $this->get($field);
         
-        $display_timezone = Configure::read('display_timezone');
+        $display_timezone = null;
+        
+        if(Configure::check('display_timezone'))
+        {
+            $display_timezone = Configure::read('display_timezone');
+        }
+        elseif(Configure::check('default_display_timezone'))
+        {
+            $display_timezone = Configure::read('default_display_timezone');
+        }
         
         if(!empty($display_timezone) && isset($value) && is_a($value, 'Cake\I18n\Time'))
         {
-            $value->setTimezone($display_timezone);
+            $value->setTimezone($display_timezone); //it doesn't change the timezone internally, but it changes the tz used for display
             
             return $value;
         }
