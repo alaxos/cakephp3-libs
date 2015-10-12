788d84620e202059e5ae09785bfb2ae7ee5a1e16
Nicolas Rod <nico@alaxos.com>
2015-10-12 11:42:39 +0200

    Added JS to close flash boxes on click

--- webroot/js/alaxos/flashMessage.js
+++ webroot/js/alaxos/flashMessage.js
@@ -1,8 +1,13 @@
-$(document).ready(function(){
-	
-	$(".flash-message-row .flash-message").click(function(e){
-		e.preventDefault();
-		$(this).hide(200);
-	});
-	
-});
+
+if (typeof(jQuery) != "undefined") {
+    
+    $(document).ready(function(){
+        
+        $(".flash-message-row .flash-message").click(function(e){
+            e.preventDefault();
+            $(this).hide(200);
+        });
+        
+    });
+    
+}
