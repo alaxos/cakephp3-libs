20e76826c61d1cc8839aceb989feb7cd66ac1755
Nicolas Rod <Nicolas.Rod@unige.ch>
2019-10-28 16:06:23 +0100

    TimezonedTrait: to_display_timezone() handles Date values in addition to Time values

--- src/Model/Entity/TimezonedTrait.php
+++ src/Model/Entity/TimezonedTrait.php
@@ -9,37 +9,19 @@ trait TimezonedTrait
 {
     public function to_display_timezone($field)
     {
-        $table = TableRegistry::getTableLocator()->get($this->_registryAlias);
-        $fieldtype = $table->getSchema()->getColumn($field)['type'];
-
         $value = $this->get($field);
 
         $display_timezone = null;
-
-        if($fieldtype == 'datetime')
-        {
-            if(Configure::check('display_timezone'))
-            {
-                $display_timezone = Configure::read('display_timezone');
-            }
-            elseif(Configure::check('default_display_timezone'))
-            {
-                $display_timezone = Configure::read('default_display_timezone');
-            }
-
-            if(!empty($display_timezone) && isset($value) && is_a($value, 'Cake\I18n\Time'))
-            {
-                $value->setTimezone($display_timezone); //it doesn't change the timezone internally, but it changes the tz used for display
-
-                return $value;
-            }
-            else
-            {
-                return $value;
-            }
+        if(Configure::check('display_timezone')) {
+            $display_timezone = Configure::read('display_timezone');
+        } elseif(Configure::check('default_display_timezone')) {
+            $display_timezone = Configure::read('default_display_timezone');
         }
-        else
-        {
+
+        if(!empty($display_timezone) && isset($value) && (is_a($value, 'Cake\I18n\Time') || is_a($value, 'Cake\I18n\Date'))) {
+            $value->setTimezone($display_timezone); //it doesn't change the timezone internally, but it changes the tz used for display
+            return $value;
+        } else {
             return $value;
         }
     }
